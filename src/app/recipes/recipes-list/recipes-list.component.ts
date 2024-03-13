ead8f48719392df4b7d366f58a0a2fb76159cc97
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-03-13 16:19:45 -0300

    Add recipe model and edited recipes

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrl: './recipes-list.component.css'
+  styleUrl: './recipes-list.component.css',
 })
 export class RecipesListComponent {
+  recipes: Recipe[] = [
+    // new Recipe(
+    //   'Test Recipe',
+    //   'This is just a test',
+    //   'https://s2-receitas.glbimg.com/dOFKHQfqtZEqGTLm9dYDWAQG75M=/0x0:1280x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_1f540e0b94d8437dbbc39d567a1dee68/internal_photos/bs/2022/r/F/8mYkcQQNuwDZBx51XtiA/manjar-receita.jpg'
+    // ),
+  ];
 
+  addRecipe() {
+    this.recipes.push(
+      new Recipe(
+        'Test Recipe',
+        'This is just a test',
+        'https://s2-receitas.glbimg.com/dOFKHQfqtZEqGTLm9dYDWAQG75M=/0x0:1280x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_1f540e0b94d8437dbbc39d567a1dee68/internal_photos/bs/2022/r/F/8mYkcQQNuwDZBx51XtiA/manjar-receita.jpg'
+      )
+    );
+  }
 }
