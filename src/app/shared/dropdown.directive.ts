a5398881872513ea7a9bdf62b7fb3a09fdaaa1f1
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-04-01 16:56:50 -0300

    Adiciona diretiva customizada

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,20 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click') onClick() {
+    this.isOpen = !this.isOpen;
+  }
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+}
