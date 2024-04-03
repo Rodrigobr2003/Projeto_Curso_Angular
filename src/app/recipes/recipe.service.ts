a0054c6b10e538fc5f31518739dd7b3b21a0e2f7
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-04-03 17:20:45 -0300

    Adds recipe service logic

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  private recipes: Recipe[] = [
+    // new Recipe(
+    //   'Test Recipe',
+    //   'This is just a test',
+    //   'https://s2-receitas.glbimg.com/dOFKHQfqtZEqGTLm9dYDWAQG75M=/0x0:1280x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_1f540e0b94d8437dbbc39d567a1dee68/internal_photos/bs/2022/r/F/8mYkcQQNuwDZBx51XtiA/manjar-receita.jpg'
+    // ),
+  ];
+
+  constructor() {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,21 +1,16 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrl: './recipes-list.component.css',
 })
-export class RecipesListComponent {
-  recipes: Recipe[] = [
-    // new Recipe(
-    //   'Test Recipe',
-    //   'This is just a test',
-    //   'https://s2-receitas.glbimg.com/dOFKHQfqtZEqGTLm9dYDWAQG75M=/0x0:1280x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_1f540e0b94d8437dbbc39d567a1dee68/internal_photos/bs/2022/r/F/8mYkcQQNuwDZBx51XtiA/manjar-receita.jpg'
-    // ),
-  ];
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [];
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  constructor(private recipeService: RecipeService) {}
 
   addRecipe() {
     this.recipes.push(
@@ -27,7 +22,7 @@ export class RecipesListComponent {
     );
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 }
